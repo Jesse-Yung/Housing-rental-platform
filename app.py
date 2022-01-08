e0aee02ed58e80a145f6c37c85f65273c35ae8bf
Snoopy Yung <snoopy.yung@fillmula.com>
2022-01-08 14:53:44 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+from datetime import datetime
+from enum import Enum
+from jsonclasses import jsonclass, jsonenum, types
+from jsonclasses_pymongo import pymongo
+from jsonclasses_server import api, authorized, server
+
+
+
+@jsonenum
+class Sex(int, Enum):
+    MALE = 1
+    FEMALE = 2
+
+
+@jsonenum
+class HouseLevel(int, Enum):
+    ONE = 1
+    TWO = 2
+    THREE = 3
+
+
+@authorized
+@api(enable='U')
+@pymongo
+@jsonclass(can_update=types.getop.isthis)
+class Admin:
+    id: str = types.readonly.str.primary.mongoid.required
+    username: str = types.str.authidentity.writenonnull.required
+    password: str = types.str.writeonly.writenonnull.salt.authbycheckpw.unqueryable.required
+    created_at: datetime = types.readonly.datetime.tscreated.required
+    updated_at: datetime = types.readonly.datetime.tsupdated.required
+
+
+@authorized
+@api
+@pymongo
+@jsonclass(
+    can_update=types.oneisvalid([
+        types.getop.isthis,
+        types.getop.isobjof('Admin')
+    ]),
+    can_delete=types.getop.isthis
+)
+class User:
+    id: str = types.readonly.str.primary.mongoid.required
+    username: str = types.str.authidentity.writenonnull.required
+    phone_number: str = types.str.unique.authidentity.writenonnull.required
+    password: str = types.str.writeonly.writenonnull.salt.authbycheckpw.unqueryable.required
+    sex: Sex | None = types.enum(Sex).writeonce
+    enable: bool = types.bool.default(False).canu(types.getop.isobjof('Admin')).required
+    house_levels: HouseLevel | None = types.enum(HouseLevel).canu(types.getop.isobjof('Admin'))
+    review_material: ReviewMaterial | None = types.objof('ReviewMaterial')
+    created_at: datetime = types.readonly.datetime.tscreated.required
+    updated_at: datetime = types.readonly.datetime.tsupdated.required
+
+
+@api
+@pymongo
+@jsonclass
+class ReviewMaterial:
+    id: str = types.readonly.str.primary.mongoid.required
+    name: str = types.str.required
+    phone_number: str = types.str.alnum.required
+    id_number: str = types.str.length(18).required
+
+    
+    created_at: datetime = types.readonly.datetime.tscreated.required
+    updated_at: datetime = types.readonly.datetime.tsupdated.required
+
+
+app = server()
